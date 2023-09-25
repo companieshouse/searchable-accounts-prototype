168f428daa0db30c826c0b46fce5882da5174e1d
mtuffs <mtuffs@companieshouse.gov.uk>
2023-09-25 17:32:47 +0100

    new accordion style added

---
+++ app/assets/javascripts/accordion-elements.js
@@ -0,0 +1,43 @@
+
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
+
+
+// NOT FOR PRODUCTION - this code is not accessible
+
+function toggleAccordion(accordion) {
+    const content = accordion.nextElementSibling;
+    const symbol = accordion.querySelector('.toggle-symbol');
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        symbol.textContent = '+';
+    } else {
+        content.style.display = 'block';
+        symbol.textContent = '-';
+    }
+}
+
+document.getElementById('open-all-link').addEventListener('click', function (e) {
+    e.preventDefault();
+    const accordions = document.querySelectorAll('.accordion');
+    const openAllLink = document.getElementById('open-all-link');
+    if (openAllLink.textContent === 'Open all') {
+        accordions.forEach((accordion) => {
+            const content = accordion.nextElementSibling;
+            const symbol = accordion.querySelector('.toggle-symbol');
+            content.style.display = 'block';
+            symbol.textContent = '-';
+        });
+        openAllLink.textContent = 'Close all';
+    } else {
+        accordions.forEach((accordion) => {
+            const content = accordion.nextElementSibling;
+            const symbol = accordion.querySelector('.toggle-symbol');
+            content.style.display = 'none';
+            symbol.textContent = '+';
+        });
+        openAllLink.textContent = 'Open all';
+    }
+});
